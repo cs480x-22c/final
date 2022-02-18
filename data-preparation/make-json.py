9f1f9e49727c1e2fd0442a74d409f0b5c8d56ce9
vrmelican <vrmelican@wpi.edu>
2022-02-18 15:34:03 -0500

    Started validating data

---
+++ data-preparation/make-json.py
@@ -0,0 +1,55 @@
+#create geojson from paths and path mappings
+
+import pandas as pd
+import json
+
+#convert numbers to latitude and longitude
+def convertToLon(n, constellation):
+    constellationData = mappings[mappings["Constellation"] == constellation]
+    data = constellationData.loc[constellationData["Number"] == n]
+    return data.iloc[0].Lon
+
+def convertToLat(n, constellation):
+    constellationData = mappings[mappings["Constellation"] == constellation]
+    data = constellationData.loc[constellationData["Number"] == n]
+    return data.iloc[0].Lat
+
+def makeFeature(constellation):    
+    constellationData = paths[paths["Name"] == constellation]
+    
+    typeField = "Feature"
+    idField = names.loc[names["Name"] == constellation].iloc[0].Abbreviation
+    nameField = constellation
+    geometryType = "MultiLineString"
+    
+    geometryCoordinates = []
+    for index, row in constellationData.iterrows():
+        coordinate = [[row.StartLon, row.StartLat], [row.EndLon, row.EndLat]]
+        geometryCoordinates.append(coordinate)
+    
+    properties = {"name": nameField}
+    geometry = {"type": geometryType, "coordinates": geometryCoordinates}
+    
+    feature = {"type": typeField, "id": idField, "properties": properties, "geometry": geometry}
+    return feature
+
+def makeGeoJSON(file, names):
+    features = []
+    for name in names:
+        feature = makeFeature(name)
+        features.append(feature)
+    data = {"type":"FeatureCollection", "features":features}
+    
+    with open('../' + file, 'w') as f:
+       json.dump(data, f)
+
+paths = pd.read_csv("paths.csv")
+mappings = pd.read_csv("path_mappings.csv")
+names = pd.read_csv("helper-csvs/names.csv")
+
+paths["StartLat"] = paths.apply(lambda x: convertToLat(x["Start"], x["Name"]), axis=1)
+paths["StartLon"] = paths.apply(lambda x: convertToLon(x["Start"], x["Name"]), axis=1)
+paths["EndLat"] = paths.apply(lambda x: convertToLat(x["End"], x["Name"]), axis=1)
+paths["EndLon"] = paths.apply(lambda x: convertToLon(x["End"], x["Name"]), axis=1)
+
+makeGeoJSON("stars.geojson", names.Name)
--- data-preparation/map-names.py
+++ data-preparation/map-names.py
@@ -11,13 +11,14 @@ manual_names = pd.read_csv("helper-csvs/manual_names.csv", encoding='latin1')
 manual_HD = pd.read_csv("helper-csvs/manual_HD.csv", encoding='latin1')
 
 #manually convert names without matches
-def replace(name):   
+def replace(name, constellation):   
     #fix typos in website
     name = name.replace("Leporus", "Leporis")
     name = name.replace("Ophichi", "Ophiuchi")
     name = name.replace("Theya", "Theta")
     
-    search = manual_names.loc[manual_names["Name"] == name]
+    filterByConstellation = manual_names.loc[manual_names["Constellation"] == constellation]
+    search = filterByConstellation.loc[filterByConstellation["Name"] == name]
     if len(search) == 1:
         search = search.iloc[0]
         return search["Name_Converted"]
@@ -64,7 +65,7 @@ def search(name, name_converted, constellation):
         searchHD = filterByConstellation.loc[filterByConstellation["Name"] == name]
         return searchHD.iloc[0].HD
        
-pathnums["Name"] = pathnums["Name"].apply(lambda x: replace(x))
+pathnums["Name"] = pathnums.apply(lambda x: replace(x["Name"], x["Constellation"]), axis=1)
 pathnums["Name_Converted"] = pathnums.apply(lambda x: convertName(x["Name"], x["Constellation"]), axis=1)
 pathnums["HD"] = pathnums.apply(lambda x: search(x["Name"], x["Name_Converted"], x["Constellation"]), axis=1)
 pathnums["HD"] = pd.to_numeric(pathnums["HD"])
@@ -72,8 +73,42 @@ stars["HD"] = pd.to_numeric(stars["HD"], errors="coerce")
 stars.drop_duplicates(subset="HD", inplace=True)
 
 #create combined file
-mapping = pathnums.merge(stars, how="inner", on="HD")
-mapping = mapping[["Number", "Constellation_x", "HD", "RA", "Dec", "vis.mag."]]
-mapping.rename(columns={"Constellation_x":"Constellation", "vis.mag.":"Mag"}, inplace=True)
+mappings = pathnums.merge(stars, how="inner", on="HD")
+mappings = mappings[["Number", "Constellation_x", "HD", "RA", "Dec", "vis.mag."]]
+mappings.rename(columns={"Constellation_x":"Constellation", "vis.mag.":"Mag"}, inplace=True)
 
-mapping.to_csv("path_mappings.csv", index=False)
+def raToDegree(ra): 
+    hours = float(ra.split("h")[0])
+    rest = ra.split("h")[1]
+    
+    minutes = float(rest.split("m")[0])
+    rest = rest.split("m")[1]
+    
+    seconds = float(rest.split("s")[0])
+    
+    decimal = hours * 15 + minutes * 15 / 60 + seconds * 15 / 3600
+    
+    return -(decimal - 180)
+
+def decToDegree(dec):
+    dec = dec.replace("+", "")
+    dec = dec.replace("−", "-")
+    
+    degree = float(dec.split("°")[0])
+    rest = dec.split("°")[1]
+    
+    minutes = float(rest.split("′")[0])
+    rest = rest.split("′")[1]
+    
+    seconds = float(rest.split("″")[0])
+    
+    decimal = degree + minutes / 60 + seconds / 3600
+    
+    return decimal
+
+#convert to latitude and longitude
+mappings["Lon"] = mappings.RA.apply(lambda x: raToDegree(x))
+mappings["Lat"] = mappings.Dec.apply(lambda x: decToDegree(x))
+mappings.drop(["RA", "Dec"], axis=1, inplace=True)
+
+mappings.to_csv("path_mappings.csv", index=False)
