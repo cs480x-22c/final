7ec6418cfa1a2751cc4fc33a23be359db5129386
carleygilmore3@gmail.com <40573151+cgilmore18@users.noreply.github.com>
2022-02-26 15:05:13 -0500

    buildings colored with variables for specified date and time vars /Users/carleygilmore/Desktop/final/renderMap.js

--- renderMap.js
+++ renderMap.js
@@ -1,3 +1,4 @@
+
 Promise.all(
   [d3.json('map.geojson')]).then( ([buildings]) => {
   createMap(buildings)
@@ -34,6 +35,40 @@ Promise.all(
       .attr('stroke', 'steelblue')
       .attr('id', d => d.properties.name)
       .attr('fill', "white");
+
+var selectedDate = "2/26/2022"
+var selectedStartHour = "1:00 PM"
+
+d3.csv("updated_25data.csv").then(function(data) {
+
+var filteredSelection = data.filter( function(d){return d.Day === selectedDate && d.Event_Start === selectedStartHour} )
+console.log(filteredSelection)
+console.log(filteredSelection.length)
+
+var buildingIDArray = ["AK", "FL", "KH", "SL", "WB", "UH", "SH", "AH", "RILEY", "FBC", "WEST", "HA", "IS", "RC", "QUAD", "HL", "CC", "OH", "GH", "HH", "MORGAN", "CRC", "AF", "GP", "RT", "OASIS"]
+var akData, flData, khData, slData, wbData, uhData, shData, ahData, rileyData, westData, haData, isData, rcData, quadData, hlData, ccData, ohData, ghData, hhData, morganData, crcData, afData, gpData, rtData, oasisData;
+var buildingDataArray = [akData, flData, khData, slData, wbData, uhData, shData, ahData, rileyData, westData, haData, isData, rcData, quadData, hlData, ccData, ohData, ghData, hhData, morganData, crcData, afData, gpData, rtData, oasisData]
+
+function generateBuildingData (idArray, dataArray) {
+for(i = 0; i < idArray.length; i++){
+  dataArray[i] = filteredSelection.filter(function(d){return d.Location1 === idArray[i]});
+}
+return dataArray
+}
+
+function colorBuildings(idArray, dataArray){
+  for(i = 0; i < idArray.length; i++){
+    if(dataArray[i].length > 1)
+    document.getElementById(idArray[i]).style.fill = "yellow";
+  }
+}
+// var rcData = filteredSelection.filter(function(d){return d.Location1 === "RC"})
+// console.log(rcData)
+// if(rcData.length > 1) document.getElementById("RC").style.fill = "yellow";
+colorBuildings(buildingIDArray, generateBuildingData(buildingIDArray, buildingDataArray));
+
+  });
+
 
 
    var mapZoom = d3.zoom()
