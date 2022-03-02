985dd3dcb82616f2c478acf2c0e3cc11aefc4f86
lewasserman <laurenewasserman@gmail.com>
2022-03-02 15:26:25 -0500

    g2

--- script.js
+++ script.js
@@ -95,6 +95,58 @@ function makeNodeLinkGraph() {
 
       console.log("links: " + JSON.stringify(links));
 
+      const nodes = species.map(d => Object.create(d));
+      const index = new Map(nodes.map(d => [d.id, d]));
+      const links2 = links.map(d => Object.assign(Object.create(d), {
+        source: index.get(d.source),
+        target: index.get(d.target)
+      }));
+
+      const svg = d3.select(DOM.svg(width, height));
+
+      const layout = cola.d3adaptor(d3)
+          .size([width, height])
+          .nodes(nodes)
+          .links(links2)
+          .jaccardLinkLengths(40, 0.7)
+          .start(30);
+      
+      const link = svg.append("g")
+          .attr("stroke", "#999")
+          .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links2)
+        .enter().append("line")
+          .attr("stroke-width", d => Math.sqrt(d.value));
+
+      const node = svg.append("g")
+          .attr("stroke", "#fff")
+          .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .enter().append("circle")
+          .attr("r", 5)
+          .attr("fill", d => color(d.group))
+          .call(layout.drag);
+
+      node.append("title")
+          .text(d => d.id);
+
+      layout.on("tick", () => {
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+      });
+
+      invalidation.then(() => layout.stop());
+
+      return svg.node();
     });
     
   
@@ -105,58 +157,58 @@ function makeNodeLinkGraph() {
   //   target: index.get(d.target)
   // }));
 
-  const nodes = species.map(d => Object.create(d));
-  const index = new Map(nodes.map(d => [d.id, d]));
-  const links2 = links.map(d => Object.assign(Object.create(d), {
-    source: index.get(d.source),
-    target: index.get(d.target)
-  }));
+  // const nodes = species.map(d => Object.create(d));
+  // const index = new Map(nodes.map(d => [d.id, d]));
+  // const links2 = links.map(d => Object.assign(Object.create(d), {
+  //   source: index.get(d.source),
+  //   target: index.get(d.target)
+  // }));
 
-  const svg = d3.select(DOM.svg(width, height));
+  // const svg = d3.select(DOM.svg(width, height));
 
-  const layout = cola.d3adaptor(d3)
-      .size([width, height])
-      .nodes(nodes)
-      .links(links2)
-      .jaccardLinkLengths(40, 0.7)
-      .start(30);
+  // const layout = cola.d3adaptor(d3)
+  //     .size([width, height])
+  //     .nodes(nodes)
+  //     .links(links2)
+  //     .jaccardLinkLengths(40, 0.7)
+  //     .start(30);
   
-  const link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links2)
-    .enter().append("line")
-      .attr("stroke-width", d => Math.sqrt(d.value));
+  // const link = svg.append("g")
+  //     .attr("stroke", "#999")
+  //     .attr("stroke-opacity", 0.6)
+  //   .selectAll("line")
+  //   .data(links2)
+  //   .enter().append("line")
+  //     .attr("stroke-width", d => Math.sqrt(d.value));
 
-  const node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", d => color(d.group))
-      .call(layout.drag);
+  // const node = svg.append("g")
+  //     .attr("stroke", "#fff")
+  //     .attr("stroke-width", 1.5)
+  //   .selectAll("circle")
+  //   .data(nodes)
+  //   .enter().append("circle")
+  //     .attr("r", 5)
+  //     .attr("fill", d => color(d.group))
+  //     .call(layout.drag);
 
-  node.append("title")
-      .text(d => d.id);
+  // node.append("title")
+  //     .text(d => d.id);
 
-  layout.on("tick", () => {
-    link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+  // layout.on("tick", () => {
+  //   link
+  //       .attr("x1", d => d.source.x)
+  //       .attr("y1", d => d.source.y)
+  //       .attr("x2", d => d.target.x)
+  //       .attr("y2", d => d.target.y);
 
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-  });
+  //   node
+  //       .attr("cx", d => d.x)
+  //       .attr("cy", d => d.y);
+  // });
 
-  invalidation.then(() => layout.stop());
+  // invalidation.then(() => layout.stop());
 
-  return svg.node();
+  // return svg.node();
 }
 
 function addLinks(d, type) {
