cfc2541cb56ac78ff0d48a84f643abaa4398f75f
Drew Fisher <awfisher@wpi.edu>
2022-03-03 16:35:27 -0500

    Add parallel plots (axes lines not working?)

--- src/components/ParallelPlot.svelte
+++ src/components/ParallelPlot.svelte
@@ -2,16 +2,32 @@
 	import * as d3 from 'd3';
 	import { onMount } from 'svelte';
 
-    let dimensions, x, y;
+	export let domain;
+	export let range;
+	let x, y, margin, width, height;
+	let svg;
+	export let data;
+    let oldlines;
 
+	let mounted = false;
 	onMount(() => {
+		mounted = true;
+		setupAxes();
+	});
+
+	$: if (mounted && data) {
+        if(oldlines) oldlines.remove();
+		oldlines = drawlines(data);
+	}
+
+	function setupAxes() {
 		// set the dimensions and margins of the graph
-		var margin = { top: 30, right: 10, bottom: 10, left: 0 },
-			width = 1000 - margin.left - margin.right,
-			height = 400 - margin.top - margin.bottom;
+		margin = { top: 30, right: 10, bottom: 10, left: 0 };
+		width = 500 - margin.left - margin.right;
+		height = 300 - margin.top - margin.bottom;
 
 		// append the svg object to the body of the page
-		var svg = d3
+		svg = d3
 			.select('#my_dataviz')
 			.append('svg')
 			.attr('width', width + margin.left + margin.right)
@@ -19,78 +35,65 @@
 			.append('g')
 			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-		// Parse the Data
-		d3.csv(
-			'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv',
-			function (data) {
-                console.log(Object.keys(data[0]))
-				dimensions = Object.keys(data[0]).filter(function (d) {
-					return d;
-				});
+		// For each dimension, I build a linear scale. I store all in a y object
+		y = {};
+		for (let axis in domain) {
+			console.log(domain[axis]);
+			y[domain[axis]] = d3.scaleLinear().domain(range).range([height, 0]);
+		}
 
-				// For each dimension, I build a linear scale. I store all in a y object
-				var y = {};
-				for (let i in dimensions) {
-					let name = dimensions[i];
-					y[name] = d3
-						.scaleLinear()
-						.domain(
-							d3.extent(data, function (d) {
-								return +d[name];
+		// Build the X scale -> it find the best position for each Y axis
+		x = d3.scalePoint().range([0, width]).padding(1).domain(domain);
+
+		// Draw the axis:
+		svg
+			.selectAll('myAxis')
+			// For each dimension of the dataset I add a 'g' element:
+			.data(domain)
+			.enter()
+			.append('g')
+			// I translate this element to its right position on the x axis
+			.attr('transform', (d) => `translate(${x(d)})`)
+			// And I build the axis with the call function
+			.each((d) => d3.select(this).call(d3.axisLeft().scale(y[d])))
+			// Add axis title
+			.append('text')
+			.style('text-anchor', 'middle')
+			.attr('y', -9)
+			.text((d) => d)
+			.style('fill', 'black');
+	}
+
+	function drawlines(data) {
+		if (svg && data.length > 0) {
+			// Draw the lines
+			return svg
+				.selectAll('myPath')
+				.data(data)
+				.enter()
+				.append('path')
+				.attr('d', (d) =>
+					// The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+					{
+						return d3.line()(
+							domain.map((p) => {
+								return [x(p), y[p](d[p])];
 							})
-						)
-						.range([height, 0]);
-				}
-
-				// Build the X scale -> it find the best position for each Y axis
-				x = d3.scalePoint().range([0, width]).padding(1).domain(dimensions);
-
-				// The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
-				function path(d) {
-					return d3.line()(
-						dimensions.map(function (p) {
-							return [x(p), y[p](d[p])];
-						})
-					);
-				}
-
-				// Draw the lines
-				svg
-					.selectAll('myPath')
-					.data(data)
-					.enter()
-					.append('path')
-					.attr('d', path)
-					.style('fill', 'none')
-					.style('stroke', '#69b3a2')
-					.style('opacity', 0.5);
-
-				// Draw the axis:
-				svg
-					.selectAll('myAxis')
-					// For each dimension of the dataset I add a 'g' element:
-					.data(dimensions)
-					.enter()
-					.append('g')
-					// I translate this element to its right position on the x axis
-					.attr('transform', function (d) {
-						return 'translate(' + x(d) + ')';
-					})
-					// And I build the axis with the call function
-					.each(function (d) {
-						d3.select(this).call(d3.axisLeft().scale(y[d]));
-					})
-					// Add axis title
-					.append('text')
-					.style('text-anchor', 'middle')
-					.attr('y', -9)
-					.text(function (d) {
-						return d;
-					})
-					.style('fill', 'black');
-			}
-		);
-	});
+						);
+					}
+				)
+				.style('fill', 'none')
+				.style('stroke', '#69b3a2')
+				.style('opacity', 0.5);
+		}
+	}
 </script>
 
 <div id="my_dataviz" />
+
+<style>
+	#my_dataviz {
+		width: 50vw;
+		height: auto;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,54 +1,55 @@
 <script>
-    import { onMount } from 'svelte';
-    import ParallelPlot from '../components/ParallelPlot.svelte';
+	import { onMount } from 'svelte';
+	import ParallelPlot from '../components/ParallelPlot.svelte';
 	import Scatterplot from '../components/Scatterplot.svelte';
 
 	let points;
-    let data;
+	let data;
 	let progress = 0;
 
 	onMount(async () => {
-        points = await (await fetch('/video_demo.json')).json();
-    });
+		points = await (await fetch('/video_demo.json')).json();
+	});
 
+	$: if (points) {
+		data = filterTime(progress, points);
+	}
 
-    $: if(points) {
-        data = filterTime(progress, points);
-    }
-
-    function filterTime(time, data) {
-        let startTime = 1646183695000
-        return data.filter(
-            (point) => (point.timestamp < time*1000 + startTime) && (point.timestamp > time*1000 + startTime - 500))
-            .map((point) => {
-                return {
-                    x: point.tip1,
-                    y: point.tip2
-                };
-	    	});
-    }
+	function filterTime(time, data) {
+		let startTime = 1646183695000;
+		return data
+			.filter(
+				(point) =>
+					point.timestamp < time * 1000 + startTime &&
+					point.timestamp > time * 1000 + startTime - 500
+			)
+			.map((point) => {
+				return {
+					x: point.tip1,
+					y: point.tip2
+				};
+			});
+	}
 </script>
 
-<video
-	id="video"
-	controls={true}
-	bind:currentTime={progress}
-	muted={true}
-	src="/trim.5AF0A6C8-6431-41A0-A3A5-F5DF17C5E849.MOV"
-/>
+
 <br />
-
-<Scatterplot
-		{...{
-			points: data
-		}}
-
+<div id="plots">
+    <ParallelPlot data={data} domain={["x","y"]} range={[0,1500]}
+	/>
+    <Scatterplot
+    {...{
+        points: data
+    }}
+    />
+</div>
+<video
+    id="video"
+    controls={true}
+    bind:currentTime={progress}
+    muted={true}
+    src="/trim.5AF0A6C8-6431-41A0-A3A5-F5DF17C5E849.MOV"
 />
-<!-- <ParallelPlot
-	{...{
-		points: data
-	}}
-/> -->
 
 <style>
 	:global(body) {
