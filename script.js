bce37ba27e67365111795d81dbf7079497aa60cd
lewasserman <laurenewasserman@gmail.com>
2022-03-02 20:59:00 -0500

    img '

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ function makeNodeLinkGraph() {
           .call(layout.drag);
 
       node.append("title")
-		  .html(d => ("<img src=" + d.sprite + " />\n" + d.species + "\n"
+		  .html(d => ("<img src='" + d.sprite + "' />\n" + d.species + "\n"
 		  + d.typeA + "/" + d.typeB + "\nStats:\n  HP: " + d.baseHP + "\n  Attack: " + d.baseAttack
 		  + "\n  Defense: " + d.baseDefense + "\n  Speed: " + d.baseSpeed
 		  + "\n  Special: " + d.baseSpecial + "\n  Total: " + d.total));
