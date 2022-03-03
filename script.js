332bdac9c715186d605e8dda7a748757247e0519
hyoder <73266860+hyoder@users.noreply.github.com>
2022-03-02 20:23:12 -0500

    fixed outline

--- script.js
+++ script.js
@@ -183,14 +183,13 @@ function makeNodeLinkGraph() {
           .attr("stroke-width", d => Math.sqrt(d.value));
 
       const node = svg.append("g")
-          .attr("stroke", "#fff")
+          .attr("stroke", function(d){if(d.typeB){return color(d.typeB);} else{return color(d.typeA);}})
           .attr("stroke-width", 1.5)
         .selectAll("circle")
         .data(nodes)
         .enter().append("circle")
           .attr("r", 5)
           .attr("fill", d => color(d.typeA))
-      	  .style("stroke", function(d){if(d.typeB){return color(d.typeB);} else{return color(d.typeA);}})
           .call(layout.drag);
 
       node.append("title")
