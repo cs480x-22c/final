79089004d89586fbbd8f0fb0f946a1c8f119091b
hyoder <73266860+hyoder@users.noreply.github.com>
2022-03-02 21:59:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ function makeNodeLinkGraph() {
           .jaccardLinkLengths(55, 0.7)
       	  .defaultNodeSize(15)
       	  .avoidOverlaps(true)
-      	  .symmetricDiffLinkLengths(10)
+      	  .symmetricDiffLinkLengths(25)
           .start(30);
       
       const link = svg.append("g")
